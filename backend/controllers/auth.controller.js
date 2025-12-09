067a8a89b69483f459ca7304b4339b7872867b5d
bourraOmar <bwrtmr@gmail.com>
2025-12-09 12:21:42 +0100

    TT-22 Implement Login API with JWT generation.

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,72 @@
+const User = require("../models/User.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const generationToken = (user) => {
+  return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
+
+post.register = async (req, res, next) => {
+  try {
+    const { email, password, role, firstName, lastName } = req.body;
+
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = new User({
+      email,
+      password: hashedPassword,
+      role: role || "Driver",
+      firstName,
+      lastName,
+    });
+
+    res.status(201).json({
+      user: {
+        id: user._id,
+        role: user.role,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+post.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = generationToken(user);
+
+    res.status(201).json({
+      token,
+      user: {
+        id: user._id,
+        role: user.role,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
