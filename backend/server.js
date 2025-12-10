7d3ab26e09883c52ec353daa2a6246d6315a0786
bourraOmar <bwrtmr@gmail.com>
2025-12-10 11:59:10 +0100

    Remove commented-out vehicle and trip routes from server.js

--- backend/server.js
+++ backend/server.js
@@ -28,11 +28,6 @@ app.use(express.json());
 app.use('/api/auth', authRoutes); 
 
 
-app.use('/api/vehicles', verifyToken, protectRoute(['Admin']), vehicleRoutes); 
-
-app.use('/api/trips', tripRoutes); 
-
-app.use(errorHandler); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
