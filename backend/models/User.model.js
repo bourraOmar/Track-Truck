edc524cedf7b253b923ef29107d725b51f0697a1
bourraOmar <bwrtmr@gmail.com>
2025-12-10 11:59:19 +0100

    Fix typos and formatting in User model schema

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -2,36 +2,37 @@ const mongoose = require('mongoose');
 
 const roles = ['Admin', 'Driver'];
 
-const userSchema = new mongoose.Schema({
-  email : {
-    type : String,
-    require : true,
-    unique : true,
-    trim : true,
-    lowercase : true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: roles,
+      default: 'Driver',
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
   },
-  password : {
-    type : String,
-    required : true,
-    minlength : 6,
-  },
-  role : {
-    type : String,
-    emun : roles,
-    default : 'Driver',
-    require : true,
-  },
-  firstName : {
-    type : String,
-    require : true,
-  },
-  lastName : {
-    type : String,
-    require : true,
-  },
-  timestamps : true,
-
-})
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
