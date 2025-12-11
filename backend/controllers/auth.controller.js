7b36371aff8430e8bf3008c8d133d897e1f3eccd
bourraOmar <bwrtmr@gmail.com>
2025-12-11 11:22:20 +0100

    Refactor authentication controller to remove try-catch blocks for cleaner code

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -9,71 +9,63 @@ const generationToken = (user) => {
 };
 
 exports.register = async (req, res, next) => {
-  try {
-    const { email, password, role, firstName, lastName } = req.body;
+  const { email, password, role, firstName, lastName } = req.body;
 
-    let user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    user = new User({
-      email,
-      password: hashedPassword,
-      role: role || "Driver",
-      firstName,
-      lastName,
-    });
-
-    await user.save();
-
-    const token = generationToken(user);
-
-    res.status(201).json({
-      token,
-      user: {
-        id: user._id,
-        role: user.role,
-        email: user.email,
-        firstName: user.firstName,
-        lastName: user.lastName,
-      },
-    });
-  } catch (error) {
-    next(error);
+  let user = await User.findOne({ email });
+  if (user) {
+    return res.status(400).json({ message: "User already exists" });
   }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  user = new User({
+    email,
+    password: hashedPassword,
+    role: role || "Driver",
+    firstName,
+    lastName,
+  });
+
+  await user.save();
+
+  const token = generationToken(user);
+
+  res.status(201).json({
+    token,
+    user: {
+      id: user._id,
+      role: user.role,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    },
+  });
 };
 
 exports.login = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-    if (!user) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
-
-    const token = generationToken(user);
-
-    res.status(201).json({
-      token,
-      user: {
-        id: user._id,
-        role: user.role,
-        email: user.email,
-        firstName: user.firstName,
-        lastName: user.lastName,
-      },
-    });
-  } catch (error) {
-    next(error);
+  if (!user) {
+    return res.status(400).json({ message: "Invalid credentials" });
   }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  const token = generationToken(user);
+
+  res.status(201).json({
+    token,
+    user: {
+      id: user._id,
+      role: user.role,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    },
+  });
 };
