5b9a8f19692a5f80a5bb26db08a2ad5e00226985
bourraOmar <bwrtmr@gmail.com>
2025-12-11 11:22:54 +0100

    Add useAuth hook for accessing authentication context

---
+++ frontend/src/hooks/useAuth.js
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error(
+      "useAuth doit être utilisé à l'intérieur d'un AuthProvider"
+    );
+  }
+
+  return context;
+};
+
+export default useAuth;
