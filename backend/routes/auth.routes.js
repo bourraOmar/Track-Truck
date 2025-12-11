cac1a86bb2279fdcda7464bf41d7e1f5c22d3cec
bourraOmar <bwrtmr@gmail.com>
2025-12-11 11:22:26 +0100

    Refactor authentication routes to use catchHandler for improved error handling

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const { register, login } = require('../controllers/auth.controller');
+const express = require("express");
+const { register, login } = require("../controllers/auth.controller");
+const { catchHandler } = require("../utils/catchAsync");
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login',login);
+router.post("/register", catchHandler(register));
+router.post("/login", catchHandler(login));
 
 module.exports = router;
