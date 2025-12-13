ed4d8a00250846b9692ae1d95018488cc5656df4
bourraOmar <bwrtmr@gmail.com>
2025-12-13 19:15:31 +0100

    Add driver management routes for retrieving and creating drivers

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { register, login } = require("../controllers/auth.controller");
+const { register, login, getAllDrivers, createDriver } = require("../controllers/auth.controller");
 const { catchHandler } = require("../utils/catchAsync");
+const { verifyToken, protectRoute } = require("../middleware/auth.middleware");
 
 const router = express.Router();
 
-router.post("/register", catchHandler(register));
+// router.post("/register", catchHandler(register)); // Disabled public registration
 router.post("/login", catchHandler(login));
+router.get("/drivers", verifyToken, protectRoute(['Admin']), catchHandler(getAllDrivers));
+router.post("/drivers", verifyToken, protectRoute(['Admin']), catchHandler(createDriver));
 
 module.exports = router;
